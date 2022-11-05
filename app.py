497e5e17edf554b79d607155e015ed4b9393f6da
Dawid Kosiński <tofik1432@gmail.com>
2022-11-05 17:02:46 +0100

    delete param

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from connexion.resolver import MethodViewResolver
 from src.utils.specification import get_bundled_specs
 
 
-def create_app(config: str = None) -> connexion.App:
+def create_app() -> connexion.App:
     app = connexion.FlaskApp(__name__, specification_dir='openapi/')
 
     # One big api YML
